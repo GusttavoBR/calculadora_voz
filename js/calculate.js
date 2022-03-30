e5f14cb9e9863514754a4f52214eabff2c23200a
GusttavoBR <90873790+GusttavoBR@users.noreply.github.com>
2022-03-29 22:34:23 -0300

    adicionando resultados inteiros e flutuantes

--- js/calculate.js
+++ js/calculate.js
@@ -2,22 +2,23 @@ import { hasOperation } from './checkOperation.js'
 const output = document.querySelector('#output');
 
 export const getResultFromVoice = (text) => {
-  const operation = hasOperation(text)
-  if (!operation) return output.textContent = `Operação não compreendida, o escutado foi: ${text}`
-  const result = calculate(text, operation)
-  return output.textContent = `O resultado é ${result.toFixed(2)}`;
+    const operation = hasOperation(text)
+    if (!operation) return output.textContent = `Operação não compreendida, o escutado foi: ${text}`
+    const result = calculate(text, operation)
+    const formatedNumber = result.toString().includes(".") ? result.toFixed(2) : result
+    return output.textContent = `O resultado é ${formatedNumber}`
 }
 
 const calculate = (text, operation) => {
-  const teste = text.split(operation)
-  switch (operation) {
-    case '+':
-      return Number(teste[0]) + Number(teste[1])
-    case '-':
-      return Number(teste[0]) - Number(teste[1])
-    case '/':
-      return Number(teste[0]) / Number(teste[1])
-    case 'x':
-      return Number(teste[0]) * Number(teste[1])
-  }
+    const teste = text.split(operation)
+    switch (operation) {
+        case '+':
+            return Number(teste[0]) + Number(teste[1])
+        case '-':
+            return Number(teste[0]) - Number(teste[1])
+        case '/':
+            return Number(teste[0]) / Number(teste[1])
+        case 'x':
+            return Number(teste[0]) * Number(teste[1])
+    }
 }
